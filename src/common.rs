894edb43c6adef17a07f758c0c6197e11169eab4
XKongJian <156444233+XKongJian@users.noreply.github.com>
2024-07-10 06:04:16 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -891,12 +891,12 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://desk.bbhcar.com:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
     let url = get_api_server(api, custom);
-    if url.is_empty() || url.contains("rustdesk.com") {
+    if url.is_empty() || url.contains("desk.bbhcar.com") {
         return "".to_owned();
     }
     format!("{}/api/audit/{}", url, typ)
