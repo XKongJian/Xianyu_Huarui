cf4ef788e6bb761e1845fdd147db59ea5410c0e5
XKongJian <156444233+XKongJian@users.noreply.github.com>
2024-07-10 13:27:26 +0800

    Update desktop_setting_page.dart

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1810,7 +1810,7 @@ class _AboutState extends State<_About> {
                           .marginSymmetric(vertical: 4.0)),
                   InkWell(
                       onTap: () {
-                        launchUrlString('https://rustdesk.com/privacy.html');
+                        launchUrlString('https://www.bbhcar.com/');
                       },
                       child: Text(
                         translate('Privacy Statement'),
@@ -1818,7 +1818,7 @@ class _AboutState extends State<_About> {
                       ).marginSymmetric(vertical: 4.0)),
                   InkWell(
                       onTap: () {
-                        launchUrlString('https://rustdesk.com');
+                        launchUrlString('https://www.bbhcar.com/');
                       },
                       child: Text(
                         translate('Website'),
